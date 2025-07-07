c35d47175bd016a4e619dec8386f6d7723d238e8
liamhigh <liamhigh78@gmail.com>
2025-07-07 03:31:28 +0200

    Add missing App.js to fix build

---
+++ verum-upload-portal/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2em' }}>
+      <h1>Verum Upload Portal</h1>
+      <p>System online.</p>
+    </div>
+  );
+}
+
+export default App;
